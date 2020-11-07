f844915216bb644ca25da37073e2f2076487b72c
farhadbhuiyan <farhadbhuiyan.cse@gmail.com>
2020-11-07 21:13:19 +0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Farhad Bhuiyan-portfolio
-
---
+++ precache-manifest.85af77d325370b9b1cab4420b2cb4a6d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ebd6926af937bc3cee42196645a956",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1906903a64a4fc477dca",
+    "url": "/portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "01a28f45c646e89fdc20",
+    "url": "/portfolio/static/css/main.b5344814.chunk.css"
+  },
+  {
+    "revision": "1906903a64a4fc477dca",
+    "url": "/portfolio/static/js/2.b2746934.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/portfolio/static/js/2.b2746934.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01a28f45c646e89fdc20",
+    "url": "/portfolio/static/js/main.0c0a690a.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "841a495bae8ce930388ebe13b2369f26",
+    "url": "/portfolio/static/media/banglademy .841a495b.png"
+  },
+  {
+    "revision": "4974f97990944496beddc2f3a3b568c9",
+    "url": "/portfolio/static/media/creative-agency -addservice.4974f979.png"
+  },
+  {
+    "revision": "c2cae95403eab79c64bb044b384e7087",
+    "url": "/portfolio/static/media/creative-agency -login.c2cae954.png"
+  },
+  {
+    "revision": "15b187fd46414042319ab0ecf9a4ae25",
+    "url": "/portfolio/static/media/creative-agency.15b187fd.png"
+  },
+  {
+    "revision": "b3a9f60158509f5fef90e8e80abb8365",
+    "url": "/portfolio/static/media/earth.b3a9f601.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "cc925e9f9ce2abdaa06a40b210a5a514",
+    "url": "/portfolio/static/media/mypic.cc925e9f.png"
+  },
+  {
+    "revision": "6748624c5b448716fae355c7a41b5f24",
+    "url": "/portfolio/static/media/volunteer-network.6748624c.png"
+  }
+]);
