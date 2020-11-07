1a758459e194803732e8994341493d87142dd9f0
farhadbhuiyan <farhadbhuiyan.cse@gmail.com>
2020-11-07 20:49:41 +0600

    initial commit

---
+++ src/components/blog.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Blog extends React.Component {
+  render() {
+    return (
+      <section id="blog" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Blog</h3>
+                <div className="line-mf"></div>
+                <h4>Coming Soon...</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Blog;
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+
+//import creativeAgency
+import creativeAgency from "../img/creative-agency.png";
+import creativeAgencyLogin from "../img/creative-agency -login.png";
+import creativeAgencyAddService from "../img/creative-agency -addservice.png";
+import banglademy from "../img/banglademy .png";
+import volunteerNetwork from "../img/volunteer-network.png";
+
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  Some Sample of my Works
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={creativeAgency} data-lightbox="gallery-vmarine">
+                  <div className="work-img card-img-top">
+                    <img src={creativeAgency} alt="" className="img-fluid w-100" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Creative Agency</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap ReactJS MongoDb 
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={creativeAgencyLogin}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={creativeAgencyAddService}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={volunteerNetwork} data-lightbox="gallery-vmarine">
+                  <div className="work-img card-img-top">
+                    <img src={volunteerNetwork} alt="" className="img-fluid w-100" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">volunteer Network</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap ReactJS MongoDb 
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={creativeAgencyLogin}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={creativeAgencyAddService}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={banglademy} data-lightbox="gallery-vmarine">
+                  <div className="work-img card-img-top">
+                    <img src={banglademy} alt="" className="img-fluid w-100" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Banglademy</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            HTML5 CSS3 Bootstrap ReactJS  
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={creativeAgencyLogin}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={creativeAgencyAddService}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
